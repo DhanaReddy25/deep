22253e01e5e9c2df73923ebbe6871b0e30a7217e
DhanaReddy25 <dhanalaxmi9262@gmail.com>
2025-05-16 19:05:20 +0530

    Update background.js

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed');
 });
 
-// Listen for messages from the popup
+// Listen for messages from  popup
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log('Message received in background:', message);
   return true;
